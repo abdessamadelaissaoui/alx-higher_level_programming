b5edfabab8fdb7894c5f113a8e85594099ae703a
abdessamadelaissaoui <abdessamadelaissaoui02@gmail.com>
2023-11-12 23:46:49 -0800

    0x02 python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+if __name__ == '__main__':
+    from sys import argv
+
+    sum = 0
+    length = len(argv)
+    for i in range(1, length):
+        sum += int(argv[i])
+    print(sum)
